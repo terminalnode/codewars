103b3dd2897c6cd1ffb409e1f3b8fca23e27c8ee
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-08-27 22:48:49 +0200

    working prototype, but it's too slow

--- codewars/count-squares-in-chess-board/count.py
+++ codewars/count-squares-in-chess-board/count.py
@@ -10,7 +10,46 @@
 # Find all sub-squares that has only ones and return
 # a dictionary of the form: {2:3, 3:1} (for the square above)
 
-from collections import deque
+def validate_point(chessBoard, xcoord, ycoord, maxcoord):
+    """See what the largest possible square from a certain point is."""
+    for i in range(maxcoord - 1):
+        summed = 0
+
+        width = maxcoord - i
+        for row in chessBoard[xcoord : xcoord + width]:
+            summed += sum(row[ycoord : ycoord + width])
+            if summed == width*width:
+                return width
+    return None
 
 def count(chessBoard):
-    return {} # Placeholder
+    result = dict()
+    dimensions = len(chessBoard)
+    points = list()
+
+    for x in range(dimensions - 1):
+        for y in range(dimensions - 1):
+            max = min(dimensions - x, dimensions - y)
+            biggest_square = validate_point(chessBoard, x, y, max)
+            if biggest_square != None:
+                for i in range(2, biggest_square + 1):
+                    if i not in result:
+                        result[i] = 1
+                    else:
+                        result[i] += 1
+
+    return result
+
+if __name__ == '__main__':
+    chess_board = [
+        [1,1,1,1],
+        [1,0,1,1],
+        [1,0,1,1],
+        [1,1,1,1]
+    ]
+    chess_board = [
+        [1,1,1],
+        [1,1,1],
+        [1,1,1]
+    ]
+    count(chess_board)

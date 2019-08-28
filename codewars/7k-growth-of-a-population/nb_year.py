578ee8d213ac9a4163fbfdc2206b76893f5d00bf
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-08-28 21:29:14 +0200

    codewars/nb_year: added description and link to start of file

--- codewars/7k-growth-of-a-population/nb_year.py
+++ codewars/7k-growth-of-a-population/nb_year.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+# https://www.codewars.com/kata/growth-of-a-population/python
+
+# p0 is starting population
+# percent is how much the population is growing each year
+# aug is how many people arrive or leave the town apart from regular growth
+# p is the target population size
+# 
+# The function returns the first year after which the population size
+# will have become equal or greater than the target population size.
 
 def nb_year(p0, percent, aug, p):
     year = 0

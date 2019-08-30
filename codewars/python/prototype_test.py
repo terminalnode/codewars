d96fc8833f5f7158d45adab77c71f149dad5a594
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-08-30 23:39:40 +0200

    codewars/python: added a template for creating unittests

---
+++ codewars/python/prototype_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import unittest
+# from count import count
+
+class TestCount(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(function(), correct_answer, "You did bad!")
+
+if __name__ == '__main__':
+    unittest.main()

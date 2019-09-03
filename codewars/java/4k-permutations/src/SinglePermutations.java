e67d8bb1a1709773d92923ee7d3f18512074cc9c
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-09-03 11:59:55 +0200

    codewars/java/4k-permutations completed

---
+++ codewars/java/4k-permutations/src/SinglePermutations.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Permutations {
+    public static List<String> singlePermutations(String s) {
+        Set<String> allPermutations = new HashSet<>();
+        getPermutations(new StringBuilder(), allPermutations, new StringBuilder(s));
+        return new ArrayList<>(allPermutations);
+    }
+
+    public static void getPermutations(StringBuilder string, Set<String> allPermutations, StringBuilder inputString) {
+        if (inputString.length() == 0) {
+            allPermutations.add(string.toString());
+        } else {
+            for (int i = 0; i < inputString.length(); i++) {
+                // Copy current string.
+                StringBuilder newString = new StringBuilder(string.toString())
+                        .append(inputString.toString().charAt(i));
+                StringBuilder newInputString = new StringBuilder(inputString.toString()).deleteCharAt(i);
+
+                // Repeat until done.
+                getPermutations(newString, allPermutations, newInputString);
+            }
+        }
+    }
+}

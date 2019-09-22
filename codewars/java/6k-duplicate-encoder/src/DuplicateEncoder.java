124e155a7ba06fe40eecbd7fb5fd52d140e121c5
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-09-22 13:53:51 +0200

    IntStreamlined solution a bit, added javadoc

--- codewars/java/6k-duplicate-encoder/src/DuplicateEncoder.java
+++ codewars/java/6k-duplicate-encoder/src/DuplicateEncoder.java
@@ -1,19 +1,23 @@
-/* https://www.codewars.com/kata/duplicate-encoder
- * Given a string, return another string where each character
- * has been replaced with ( if the character occurs only once
- * in the string and ) otherwise. Ignore case.
- * */
 import java.util.stream.IntStream;
+
+/**
+ * Given a string, return another string where each character
+ * has been replaced with '(' if the character occurs only once
+ * in the string and ')' otherwise. Case is ignored.
+ *
+ * https://www.codewars.com/kata/duplicate-encoder
+ *
+ * @author Alexander Rundberg
+ * @version 1.0
+ */
 
 public class DuplicateEncoder {
     static String encode(String word){
-        int[] chars =
-                toIntStream(word)
+        int[] chars = toIntStream(word)
                 .map(character ->
-                        toIntStream(word)
-                        .filter(c -> c == character)
-                        .toArray()
-                        .length)
+                        (int) toIntStream(word)
+                                .filter(c -> c == character)
+                                .count())
                 .map(num -> num == 1 ? '(' : ')')
                 .toArray();
 
@@ -22,7 +26,5 @@ public class DuplicateEncoder {
         return result.toString();
     }
 
-    static IntStream toIntStream(String word) {
-        return word.toLowerCase().chars();
-    }
+    private static IntStream toIntStream(String word) { return word.toLowerCase().chars(); }
 }

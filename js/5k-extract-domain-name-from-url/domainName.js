d4f8566a45a07aea850e39534fe14203dea61e7f
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-01-15 10:33:31 +0100

    js: 5k-extract-domain-name-from-url solved

---
+++ js/5k-extract-domain-name-from-url/domainName.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/514a024011ea4fb54200004b/
+// Extract the domain name from a URL.
+//
+function domainName(url){
+    return url.replace(/https?:\/\//g, "")
+              .replace("www.", "")
+              .replace(/\..+/g, "")
+}
+
+// ----------------------- //
+// Tests
+// ----------------------- //
+function test(input, expected) {
+    result = domainName(input)
+    
+    console.log("Testing with " + input + ".")
+    if (result === expected) {
+        console.log("Test passed!")
+    } else {
+        console.log("Test failed. :(")
+        console.log(" => Got:      " + result)
+        console.log("    Expected: " + expected)
+    }
+    console.log()
+}
+
+test("http://google.com", "google");
+test("http://google.co.jp", "google");
+test("www.xakep.ru", "xakep");
+test("https://youtube.com", "youtube");

5eb5ff675cb907bddd379086c1a966b7d48aa224
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2022-04-18 16:53:13 +0200

    go: convert tonato tests to use t.Run

--- golang/kata6k/tonato/tonato_test.go
+++ golang/kata6k/tonato/tonato_test.go
@@ -4,31 +4,39 @@ import "testing"
 
 func TestToNato(t *testing.T) {
 	cases := []struct {
-		in  string
-		out string
+		name string
+		in   string
+		out  string
 	}{
 		{
-			in:  "If you can read",
-			out: "India Foxtrot Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta",
+			name: "With all allowed special characters (,.!? and space)",
+			in:   "a, B.c?!",
+			out:  "Alfa , Bravo . Charlie ? !",
 		}, {
-			in:  "Did not see that coming",
-			out: "Delta India Delta November Oscar Tango Sierra Echo Echo Tango Hotel Alfa Tango Charlie Oscar Mike India November Golf",
+			name: "",
+			in:   "Did not see that coming",
+			out:  "Delta India Delta November Oscar Tango Sierra Echo Echo Tango Hotel Alfa Tango Charlie Oscar Mike India November Golf",
 		}, {
-			in:  "go for it!",
-			out: "Golf Oscar Foxtrot Oscar Romeo India Tango !",
+			name: "Ending in exclamation mark",
+			in:   "go for it!",
+			out:  "Golf Oscar Foxtrot Oscar Romeo India Tango !",
 		}, {
-			in:  "",
-			out: "",
+			name: "Empty string",
+			in:   "",
+			out:  "",
 		}, {
-			in:  ",",
-			out: ",",
+			name: "Just a comma",
+			in:   ",",
+			out:  ",",
 		},
 	}
 
 	for _, c := range cases {
-		actual := ToNato(c.in)
-		if actual != c.out {
-			t.Errorf("ToNato(\"%s\") returned %s, expected %s", c.in, actual, c.out)
-		}
+		t.Run(c.name, func(t *testing.T) {
+			actual := ToNato(c.in)
+			if actual != c.out {
+				t.Errorf("ToNato(\"%s\") returned %s, expected %s", c.in, actual, c.out)
+			}
+		})
 	}
 }

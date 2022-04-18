057ed9a09703ac5f6f537bc3110b93b2df34f00c
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2022-04-18 16:48:25 +0200

    retroactively add tocamelcase tests

---
+++ golang/kata6k/tocamelcase/tocamelcase_test.go
@@ -0,0 +1,26 @@
+package tocamelcase
+
+import "testing"
+
+func TestToCamelCase(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  string
+		want string
+	}{
+		{name: "lower-kebab-case", arg: "the-stealth-warrior", want: "theStealthWarrior"},
+		{name: "Upper-Kebab-Case", arg: "The-Stealth-Warrior", want: "TheStealthWarrior"},
+		{name: "Mixed-kebab-Case", arg: "The-stealth-Warrior", want: "TheStealthWarrior"},
+		{name: "lower_snake_case", arg: "the_stealth_warrior", want: "theStealthWarrior"},
+		{name: "Upper_Snake_Case", arg: "The_Stealth_Warrior", want: "TheStealthWarrior"},
+		{name: "Mixed_snake_Case", arg: "The_stealth_Warrior", want: "TheStealthWarrior"},
+		{name: "Empty string", arg: "", want: ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ToCamelCase(tt.arg); got != tt.want {
+				t.Errorf("ToCamelCase() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
